411fe6f2c2964783ef2ce6578fe785f0f08e285d
RohithPavanSai <rohithpavansai45@gmail.com>
2025-12-15 15:03:28 +0530

    new

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,40 @@
+/**
+ * HelloWorld class - A simple program demonstrating Java basics
+ */
 class HelloWorld {
+    
+    private static final String GREETING = "Hello, World!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        printGreeting();
+        printProgramInfo();
+        demonstrateLooping();
+    }
+    
+    /**
+     * Prints the greeting message
+     */
+    private static void printGreeting() {
+        System.out.println(GREETING);
+    }
+    
+    /**
+     * Prints program information
+     */
+    private static void printProgramInfo() {
+        System.out.println("\n--- Program Information ---");
+        System.out.println("Program Name: HelloWorld");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+    }
+    
+    /**
+     * Demonstrates a simple loop
+     */
+    private static void demonstrateLooping() {
+        System.out.println("\n--- Counting Loop ---");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
     }
 }
